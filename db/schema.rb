e6d56e5377fbbb70a0c8b7776486bfe96d26842d
Luiza Toledo <luiza.toledo96@gmail.com>
2020-12-13 16:08:59 -0300

    migration to new models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_183855) do
+ActiveRecord::Schema.define(version: 2020_12_13_185540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,5 +36,38 @@ ActiveRecord::Schema.define(version: 2020_12_13_183855) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "cart_skus", force: :cascade do |t|
+    t.bigint "sku_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_cart_skus_on_cart_id"
+    t.index ["sku_id"], name: "index_cart_skus_on_sku_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skus", force: :cascade do |t|
+    t.integer "sku"
+    t.integer "inventory"
+    t.float "price"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_skus_on_product_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_skus", "carts"
+  add_foreign_key "cart_skus", "skus"
+  add_foreign_key "skus", "products"
 end
