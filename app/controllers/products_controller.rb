935391914f63fcda6a7e0132b5b644ba7152ac0d
Luiza Toledo <luiza.toledo96@gmail.com>
2020-12-13 18:44:38 -0300

    basic HTML for cart display of products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :current_cart
-  
   def index
     @products = Product.all
-    @cart = current_cart
   end
-
-  private
-
-  # action to create a persistent cart using sessions cookies
-  def current_cart
-    if session[:cart]
-      @cart = Cart.find(session[:cart])
-    else
-      @cart = Cart.create
-      session[:cart] = @cart.id
-    end
-  end
-
-  # def products_in_cart
-  #   @cart = current_cart
-  #   products_info =  @cart.products.map do |product|
-  #     # {sku: product.sku.sku, amount: }
-  #   end 
-  # end
 end
