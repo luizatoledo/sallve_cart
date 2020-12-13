e7cca15a20b5dc5657e35c9798e3c2b063e4db5d
Luiza Toledo <luiza.toledo96@gmail.com>
2020-12-13 18:08:02 -0300

    starting new action to return products in cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,13 @@ class ProductsController < ApplicationController
     @products = Product.all
     @cart = current_cart
   end
+
+  private
+
+  def products_in_cart
+    @cart = current_cart
+    products_info =  @cart.products.map do |product|
+      {sku: product.sku.sku}
+    end 
+  end
 end
