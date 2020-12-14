595cdc8c63b5867e8f0d37ee328846ebdff2c3e1
Luiza Toledo <luiza.toledo96@gmail.com>
2020-12-13 22:31:56 -0300

    very simple destroy and simple add to cart features ok

--- app/controllers/cart_skus_controller.rb
+++ app/controllers/cart_skus_controller.rb
@@ -1,14 +1,18 @@
 class CartSkusController < ApplicationController
   def create
+    CartSku.where()
     @cart_sku = CartSku.new(cart_sku_params)
     @cart_sku.cart = @cart
     @cart_sku.save
+    redirect_to root_path
   end
 
   def destroy
     @cart_sku = CartSku.find(params[:id])
+    @cart_sku.destroy
     redirect_to root_path
   end
+  
   private
 
   def cart_sku_params
