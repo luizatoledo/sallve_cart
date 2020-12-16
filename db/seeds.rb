46dc796ba5d2e7a50cc929e0ef29d07f4ad8017b
Luiza Toledo <luiza.toledo96@gmail.com>
2020-12-16 13:49:24 -0300

    adjusted file path on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Product.delete_all
 
 # Sérum Antiacne
 antiacne = Product.create!(name: 'sérum antiacne')
-antiacne_pic = File.open('../app/assets/images/antiacne.jpg')
+antiacne_pic = File.open('app/assets/images/antiacne.jpg')
 antiacne.photo.attach(io: antiacne_pic, filename:'antiacne.png', content_type: 'image/png')
 
 # Antioxidante Hidratante
